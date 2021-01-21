fd1dd8aac59d857bbbd14f4bb5a34508578209b6
TheCoolDeveloper <76424367+TheCoolGDev@users.noreply.github.com>
2021-01-21 19:25:52 +0545

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -7,13 +7,12 @@ module.exports = {
         if (!rule || rule < 1 || rule > 6) return message.reply('please provide a rule number between 1 and 6.');
         
         if (rule) {
-          const adChannel = message.guilds.channels.cache.find(ch => ch.name.includes('🎫┃advertise'));
             
           if (rule === 1) return message.channel.send('We DO NOT tolerate any form of harassment, offensive language, threats, spam, personal attacks, racism, sexual content or anything else that does not follow the Discord community guidelines or TOS.');
           
           if (rule === 2) return message.channel.send('The developers are extremely busy, so please don’t spam/ping them. If you need help, please ping the admins instead so that you can get help.');
             
-          if (rule === 3) return message.channel.send(`Self promotion of content should be done in the ${adChannel}!`);
+          if (rule === 3) return message.channel.send('Self promotion of content should be done in the 🎫┃advertise channel');
 
           if (rule === 4) return message.channel.send('Don’t jump channels and annoy people, and don’t use voice changers. Be yourself. Also remember to use a channel suited to your needs.');
             
