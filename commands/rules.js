c2d770ffa94fed37141ae738dd4602500abd1a88
TheCoolDeveloper <76424367+TheCoolGDev@users.noreply.github.com>
2021-01-21 19:13:10 +0545

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -7,7 +7,7 @@ module.exports = {
         if (!rule || rule < 1 || rule > 5) return message.reply('please provide a rule number between 1 and 5.');
         
         if (rule) {
-          if (rule === 1) return message.channel.send('This is our rule no. 1.');
+          if (rule === 1) return message.channel.send('We DO NOT tolerate any form of harassment, offensive language, threats, spam, personal attacks, racism, sexual content or anything else that does not follow the Discord community guidelines or TOS.');
           
           
         }
