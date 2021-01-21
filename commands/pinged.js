bd94567c9980fb2ccf41757334eea3f9b99610b3
TheCoolDeveloper <76424367+TheCoolGDev@users.noreply.github.com>
2021-01-21 18:55:07 +0545

    Update pinged.js

--- commands/pinged.js
+++ commands/pinged.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'pinged',
     description: "this is a real ping command!",
     execute(message, args){
-        message.reply('Finding bots ping...').then(msg => {
+        message.author.reply('Finding bots ping...').then(msg => {
                 const ping = msg.createdTimestamp - message.createdTimestamp;
                 msg.edit(`TheCoolBot's ping is ${ping}`);
         })
