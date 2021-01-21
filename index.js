87f1bbfabc940804a8574417c4b69f7c36e8e35b
TheCoolDeveloper <76424367+TheCoolGDev@users.noreply.github.com>
2021-01-21 19:39:30 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
+const { Client, MessageAttachment } = require('discord.js');
+
 const prefix = '.';
 
 const fs = require('fs');
